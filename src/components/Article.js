515927650622e43536d0c2fdf52b491e8088a927
Tarbiee <dorcusstaby@gmail.com>
2023-11-12 15:24:33 +0300

    completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Article(props){
+    return(
+        <Article>
+           <h3>{props.title} </h3>
+            <small>{props.date || "January 1, 1970"} </small> 
+            <p>{props.preview}</p>
+
+        </Article>
+    )
+
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList(props){
+      return(
+        <main>
+            {props.posts.map((post)=>(
+                <Article
+                key= {post.id}
+                title={post.title}
+                date ={post.date || "January 1,1970"}
+                minutes={post.minutes}
+                preview ={post.preview}
+                />
+            ))}
+        </main>
+    );
+}
+
+export default ArticleList;
